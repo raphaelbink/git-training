8cefc218d76e4fdf92d7ecf3e78da78aa519e5ef
Anita Clement <anita.clement@saint-gobain.com>
2019-06-04 14:39:29 -0400

    yield plot

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
 import sklearn as sk
+import matplotlib.pyplot as plt
 
-data = pd.read_csv('../data/factory_data.csv')
+data = pd.read_csv('C:/Users/A5692090/Documents/PROJECTS/Git_tests/git-training/data/factory_data.csv')
 
 print(data.describe())
 print(data.shape)
 print(data['humidity'].data(df['linespeed']))
+
+data.head()
+plt.plot(data.yieldl_loss)
